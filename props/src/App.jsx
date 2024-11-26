5aa644244622309c579589fff13f30d551b8fef3
Nasirgrema <muhammadnasirg250@gmail.com>
2024-11-26 02:18:24 -0800

    feat: modifications

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -2,11 +2,10 @@ import './App.css'
 import Profile from "./components/Profile"
 function App() {
   return (
-   <div className='App'>   
+   <div className='App'>
 <Profile name = 'muhammad nasir' course= 'computer science'level = '200L' />
 <Profile name = "khadija" course= "computer science" level = "300L"/>
 <Profile name = "Aramma" course= "computer science" level = "400L"/>
-<Profile name = "Ya'u" course= "computer science" level = "100L"/>
    </div>
   )
 }
--- props/src/components/Profile.jsx
+++ props/src/components/Profile.jsx
@@ -1,8 +1,10 @@
 import students from '../Data'
+import Counter from '../counter';
 
 function Profile(data){
   return (
     <div>
+      <Counter/>
       <h3>{data.name}</h3>
       <p>{data.course}</p>
       <p>{data.level}</p>
